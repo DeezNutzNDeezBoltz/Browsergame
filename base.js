6873e26f98c3d74d332a33a4ba6bfb33c2baee4c
Zachary Wilson Bullard <mrwaynecarnage@gmail.com>
2018-11-05 06:54:26 -0600

    New

---
+++ base.js
@@ -0,0 +1,21 @@
+
+
+var express = require('express');
+var app = express();
+var serv = require('http') .Server(app);
+
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log("Game has booted! Enjoy!")
+
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket){
+    console.log('Connect Established!')
+})
+
