f21b91a3b02a936465416963ad249e672fd6111c
Bill Schumacher <bill@servernet.co>
2018-11-06 18:57:42 -0600

    Change port to 80.

--- base.js
+++ base.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(80);
 console.log("Game has booted! Enjoy!")
 
 var io = require('socket.io')(serv, {});
